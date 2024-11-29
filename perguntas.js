3970bc566054eabe8d642d4afc45b1abeeee7a01
JKZ-MILLER <juancarlosdasilva2021@gmail.com>
2024-11-29 17:43:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Naruto',
+    'Qual é o sonho de Naruto Uzumaki?',
+    'O sonho de Naruto é se tornar Hokage, o líder de sua vila, para ser respeitado por todos.'
+)
+
+criaCartao(
+    'Naruto',
+    'O que é o Rasengan?',
+    'O Rasengan é uma técnica poderosa criada por Minato Namikaze que concentra chakra em forma de esfera giratória para causar grande impacto.'
+)
+
+criaCartao(
+    'Naruto',
+    'Quem é o melhor amigo e rival de Naruto?',
+    'O melhor amigo e rival de Naruto é Sasuke Uchiha, um membro do clã Uchiha que busca vingança por sua família.'
+)
+
+criaCartao(
+    'Naruto',
+    'O que é a Kyuubi?',
+    'A Kyuubi, ou Raposa de Nove Caudas, é uma poderosa besta com cauda selada dentro de Naruto quando ele era bebê.'
+)
